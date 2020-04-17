3502be2a82ef9f95fa983ec61e275d35eda19f63
wy97 <54507081+wy97@users.noreply.github.com>
2020-04-16 23:03:04 -0500

    Update __init__.py

--- FCN/augmentation/__init__.py
+++ FCN/augmentation/__init__.py
@@ -3,6 +3,103 @@
 
 # In[ ]:
 
+import logging
+
+from FCN.augmentation.aug import (
+
+    AdjustContrast,
+
+    AdjustGamma,
+
+    AdjustBrightness,
+
+    AdjustSaturation,
+
+    AdjustHue,
+
+    RandomCrop,
+
+    RandomHorizontallyFlip,
+
+    RandomVerticallyFlip,
+
+    Scale,
+
+    RandomSized,
+
+    RandomSizedCrop,
+
+    RandomRotate,
+
+    RandomTranslate,
+
+    CenterCrop,
+
+    Compose,
+
+)
+
+
+
+logger = logging.getLogger("FCN")
+
+
+
+key2aug = {
+
+    "gamma": AdjustGamma,
+
+    "hue": AdjustHue,
+
+    "brightness": AdjustBrightness,
+
+    "saturation": AdjustSaturation,
+
+    "contrast": AdjustContrast,
+
+    "rcrop": RandomCrop,
+
+    "hflip": RandomHorizontallyFlip,
+
+    "vflip": RandomVerticallyFlip,
+
+    "scale": Scale,
+
+    "rsize": RandomSized,
+
+    "rsizecrop": RandomSizedCrop,
+
+    "rotate": RandomRotate,
+
+    "translate": RandomTranslate,
+
+    "ccrop": CenterCrop,
+
+}
+
+
+
+
+
+def get_composed_augmentations(aug_dict):
+
+    if aug_dict is None:
+
+        logger.info("Using No Augmentations")
+
+        return None
+
+
+
+    augmentations = []
+
+    for aug_key, aug_param in aug_dict.items():
+
+        augmentations.append(key2aug[aug_key](aug_param))
+
+        logger.info("Using {} aug with params {}".format(aug_key, aug_param))
+
+    return Compose(augmentations)
 
 
 
